ecc4baed2d30d3e9f279d48b2dd1a9e39fde871b
Svitlana Somina <sominasvet@gmail.com>
2022-01-11 14:43:27 +0200

    Task Automation_HW4 was done

---
+++ src/main/java/model/ColorData.java
@@ -0,0 +1,29 @@
+package model;
+
+public class ColorData {
+    private Integer id;
+    private String name;
+    private Integer year;
+    private String color;
+    private String pantone_value;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getPantone_value() {
+        return pantone_value;
+    }
+}
